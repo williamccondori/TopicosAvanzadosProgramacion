497fdd431663f60b7e145e56b75b1a070a3d08e9
William Condori <williamccondori@outlook.com>
2017-10-28 20:49:41 -0500

    Operaciones con colas

--- TopicosAvanzadosProgramacion/stacks_queues/test_queues.py
+++ TopicosAvanzadosProgramacion/stacks_queues/test_queues.py
@@ -1,23 +1,23 @@
 import unittest
 
-class Pila:
+class Cola:
     def __init__(self, tamanio):
         self.tamanio = tamanio
         self.items = []
-    def is_empty(self):
-        return self.items == []
-    def push(self, item):
+    def enqueue(self, item):
         if not self.is_full():
             self.items.insert(len(self.items), item)
-    def pop(self):
-        return self.items.pop(len(self.items) - 1)
+    def is_empty(self):
+        return self.items == []
+    def front(self):
+        return self.items[0]
+    def dequeue(self):
+        if not self.is_empty():
+            self.items.pop(0)
     def is_full(self):
         return len(self.items) >= self.tamanio
     def get_size(self):
         return len(self.items)
-    def top(self):
-        posicion_ultimo = len(self.items) - 1
-        return self.items[posicion_ultimo]
 
     def get(self):
         """
@@ -25,37 +25,37 @@ class Pila:
         """
         return self.items
 
-class TestStack(unittest.TestCase):
-    def test_crear_pila(self):
+class TestQueue(unittest.TestCase):
+    def test_crear_cola(self):
         comprobador = ['Manzana', 'Pera', 'Banana', 'Lima', 'Naranjas'] # Este debe de ser el resultado.
         tamanio = 5
-        pila = Pila(tamanio)
-        pila.push('Manzana')
-        pila.push('Pera')
-        pila.push('Banana')
-        pila.push('Lima')
-        pila.push('Naranjas')
-        self.assertEqual(pila.get(), comprobador)
-    def test_elimnar_pila(self):
-        comprobador = ['Manzana', 'Pera', 'Banana', 'Lima'] # Este debe de ser el resultado.
+        cola = Cola(tamanio)
+        cola.enqueue('Manzana')
+        cola.enqueue('Pera')
+        cola.enqueue('Banana')
+        cola.enqueue('Lima')
+        cola.enqueue('Naranjas')
+        self.assertEqual(cola.get(), comprobador)
+    def test_elimnar_cola(self):
+        comprobador = ['Pera', 'Banana', 'Lima', 'Naranjas'] # Este debe de ser el resultado.
         tamanio = 5
-        pila = Pila(tamanio)
-        pila.push('Manzana')
-        pila.push('Pera')
-        pila.push('Banana')
-        pila.push('Lima')
-        pila.push('Naranjas')
-        pila.pop()
-        self.assertEqual(pila.get(), comprobador)
+        cola = Cola(tamanio)
+        cola.enqueue('Manzana')
+        cola.enqueue('Pera')
+        cola.enqueue('Banana')
+        cola.enqueue('Lima')
+        cola.enqueue('Naranjas')
+        cola.dequeue()
+        self.assertEqual(cola.get(), comprobador)
     def test_agregar_mas_tamanio(self):
         tamanio = 5
-        pila = Pila(tamanio)
-        pila.push('Manzana')
-        pila.push('Pera')
-        pila.push('Banana')
-        pila.push('Lima')
-        pila.push('Naranjas')
-        pila.push('fresa')
+        pila = Cola(tamanio)
+        pila.enqueue('Manzana')
+        pila.enqueue('Pera')
+        pila.enqueue('Banana')
+        pila.enqueue('Lima')
+        pila.enqueue('Naranjas')
+        pila.enqueue('fresa')
         self.assertEqual(pila.get_size(), tamanio)
 
 if __name__ == '__main__':
